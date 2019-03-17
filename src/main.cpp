057ca616c4cd52271f2346b4215659dab15b56ca
Ahmed Belal <ahmedbelalnour@gmail.com>
2019-03-17 21:10:01 +0200

    - Move the car forward in a straight line at a constant 50 MPH velocity.

--- src/main.cpp
+++ src/main.cpp
@@ -100,6 +100,14 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+		  // Start by simply trying to move the car forward in a straight line at a constant 50 MPH velocity. 
+		  double dist_inc = 0.5;
+		  for (int i = 0; i < 50; ++i) 
+		  {
+			  next_x_vals.push_back(car_x + (dist_inc*i)*cos(deg2rad(car_yaw)));
+			  next_y_vals.push_back(car_y + (dist_inc*i)*sin(deg2rad(car_yaw)));
+		  }
+
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
