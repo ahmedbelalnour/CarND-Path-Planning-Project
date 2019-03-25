331580342822461a0133247b834ac0e96c22dd33
Ahmed Belal <ahmedbelalnour@gmail.com>
2019-03-25 22:57:26 +0200

    - Adding is_target_lane_safe function to check if it's safe to change for it

--- src/helpers.h
+++ src/helpers.h
@@ -6,7 +6,8 @@
 #include <vector>
 
 #define M_PI       3.14159265358979323846   // pi
-
+#define FORWARD_SAFE_DISRANCE 35
+#define BACKWARD_SAFE_DISRANCE 25
 // for convenience
 using std::string;
 using std::vector;
@@ -14,16 +15,20 @@ using std::vector;
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 //   else the empty string "" will be returned.
-string hasData(string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("}");
-  if (found_null != string::npos) {
-    return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
-    return s.substr(b1, b2 - b1 + 2);
-  }
-  return "";
+string hasData(string s)
+{
+	auto found_null = s.find("null");
+	auto b1 = s.find_first_of("[");
+	auto b2 = s.find_first_of("}");
+	if (found_null != string::npos)
+	{
+		return "";
+	}
+	else if (b1 != string::npos && b2 != string::npos)
+	{
+		return s.substr(b1, b2 - b1 + 2);
+	}
+	return "";
 }
 
 //
@@ -37,123 +42,189 @@ double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
 // Calculate distance between two points
-double distance(double x1, double y1, double x2, double y2) {
-  return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+double distance(double x1, double y1, double x2, double y2)
+{
+	return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
 }
 
 // Calculate closest waypoint to current x, y position
-int ClosestWaypoint(double x, double y, const vector<double> &maps_x, 
-                    const vector<double> &maps_y) {
-  double closestLen = 100000; //large number
-  int closestWaypoint = 0;
+int ClosestWaypoint(double x, double y, const vector<double> &maps_x,
+	const vector<double> &maps_y)
+{
+	double closestLen = 100000; //large number
+	int closestWaypoint = 0;
 
-  for (int i = 0; i < maps_x.size(); ++i) {
-    double map_x = maps_x[i];
-    double map_y = maps_y[i];
-    double dist = distance(x,y,map_x,map_y);
-    if (dist < closestLen) {
-      closestLen = dist;
-      closestWaypoint = i;
-    }
-  }
+	for (int i = 0; i < maps_x.size(); ++i)
+	{
+		double map_x = maps_x[i];
+		double map_y = maps_y[i];
+		double dist = distance(x, y, map_x, map_y);
+		if (dist < closestLen)
+		{
+			closestLen = dist;
+			closestWaypoint = i;
+		}
+	}
 
-  return closestWaypoint;
+	return closestWaypoint;
 }
 
 // Returns next waypoint of the closest waypoint
-int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, 
-                 const vector<double> &maps_y) {
-  int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
+int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
+	const vector<double> &maps_y)
+{
+	int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
 
-  double map_x = maps_x[closestWaypoint];
-  double map_y = maps_y[closestWaypoint];
+	double map_x = maps_x[closestWaypoint];
+	double map_y = maps_y[closestWaypoint];
 
-  double heading = atan2((map_y-y),(map_x-x));
+	double heading = atan2((map_y - y), (map_x - x));
 
-  double angle = fabs(theta-heading);
-  angle = std::min(2*pi() - angle, angle);
+	double angle = fabs(theta - heading);
+	angle = std::min(2 * pi() - angle, angle);
 
-  if (angle > pi()/2) {
-    ++closestWaypoint;
-    if (closestWaypoint == maps_x.size()) {
-      closestWaypoint = 0;
-    }
-  }
+	if (angle > pi() / 2)
+	{
+		++closestWaypoint;
+		if (closestWaypoint == maps_x.size())
+		{
+			closestWaypoint = 0;
+		}
+	}
 
-  return closestWaypoint;
+	return closestWaypoint;
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, 
-                         const vector<double> &maps_x, 
-                         const vector<double> &maps_y) {
-  int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
+vector<double> getFrenet(double x, double y, double theta,
+	const vector<double> &maps_x,
+	const vector<double> &maps_y)
+{
+	int next_wp = NextWaypoint(x, y, theta, maps_x, maps_y);
 
-  int prev_wp;
-  prev_wp = next_wp-1;
-  if (next_wp == 0) {
-    prev_wp  = maps_x.size()-1;
-  }
+	int prev_wp;
+	prev_wp = next_wp - 1;
+	if (next_wp == 0)
+	{
+		prev_wp = maps_x.size() - 1;
+	}
 
-  double n_x = maps_x[next_wp]-maps_x[prev_wp];
-  double n_y = maps_y[next_wp]-maps_y[prev_wp];
-  double x_x = x - maps_x[prev_wp];
-  double x_y = y - maps_y[prev_wp];
+	double n_x = maps_x[next_wp] - maps_x[prev_wp];
+	double n_y = maps_y[next_wp] - maps_y[prev_wp];
+	double x_x = x - maps_x[prev_wp];
+	double x_y = y - maps_y[prev_wp];
 
-  // find the projection of x onto n
-  double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
-  double proj_x = proj_norm*n_x;
-  double proj_y = proj_norm*n_y;
+	// find the projection of x onto n
+	double proj_norm = (x_x*n_x + x_y * n_y) / (n_x*n_x + n_y * n_y);
+	double proj_x = proj_norm * n_x;
+	double proj_y = proj_norm * n_y;
 
-  double frenet_d = distance(x_x,x_y,proj_x,proj_y);
+	double frenet_d = distance(x_x, x_y, proj_x, proj_y);
 
-  //see if d value is positive or negative by comparing it to a center point
-  double center_x = 1000-maps_x[prev_wp];
-  double center_y = 2000-maps_y[prev_wp];
-  double centerToPos = distance(center_x,center_y,x_x,x_y);
-  double centerToRef = distance(center_x,center_y,proj_x,proj_y);
+	//see if d value is positive or negative by comparing it to a center point
+	double center_x = 1000 - maps_x[prev_wp];
+	double center_y = 2000 - maps_y[prev_wp];
+	double centerToPos = distance(center_x, center_y, x_x, x_y);
+	double centerToRef = distance(center_x, center_y, proj_x, proj_y);
 
-  if (centerToPos <= centerToRef) {
-    frenet_d *= -1;
-  }
+	if (centerToPos <= centerToRef)
+	{
+		frenet_d *= -1;
+	}
 
-  // calculate s value
-  double frenet_s = 0;
-  for (int i = 0; i < prev_wp; ++i) {
-    frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
-  }
+	// calculate s value
+	double frenet_s = 0;
+	for (int i = 0; i < prev_wp; ++i)
+	{
+		frenet_s += distance(maps_x[i], maps_y[i], maps_x[i + 1], maps_y[i + 1]);
+	}
 
-  frenet_s += distance(0,0,proj_x,proj_y);
+	frenet_s += distance(0, 0, proj_x, proj_y);
 
-  return {frenet_s,frenet_d};
+	return { frenet_s,frenet_d };
 }
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, 
-                     const vector<double> &maps_x, 
-                     const vector<double> &maps_y) {
-  int prev_wp = -1;
+vector<double> getXY(double s, double d, const vector<double> &maps_s,
+	const vector<double> &maps_x,
+	const vector<double> &maps_y) 
+{
+	int prev_wp = -1;
 
-  while (s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1))) {
-    ++prev_wp;
-  }
+	while (s > maps_s[prev_wp + 1] && (prev_wp < (int)(maps_s.size() - 1)))
+	{
+		++prev_wp;
+	}
 
-  int wp2 = (prev_wp+1)%maps_x.size();
+	int wp2 = (prev_wp + 1) % maps_x.size();
 
-  double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),
-                         (maps_x[wp2]-maps_x[prev_wp]));
-  // the x,y,s along the segment
-  double seg_s = (s-maps_s[prev_wp]);
+	double heading = atan2((maps_y[wp2] - maps_y[prev_wp]),
+		(maps_x[wp2] - maps_x[prev_wp]));
+	// the x,y,s along the segment
+	double seg_s = (s - maps_s[prev_wp]);
 
-  double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
-  double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
+	double seg_x = maps_x[prev_wp] + seg_s * cos(heading);
+	double seg_y = maps_y[prev_wp] + seg_s * sin(heading);
 
-  double perp_heading = heading-pi()/2;
+	double perp_heading = heading - pi() / 2;
 
-  double x = seg_x + d*cos(perp_heading);
-  double y = seg_y + d*sin(perp_heading);
+	double x = seg_x + d * cos(perp_heading);
+	double y = seg_y + d * sin(perp_heading);
 
-  return {x,y};
+	return { x,y };
+}
+
+bool is_target_lane_safe(const int target_lane, const int prev_size, const double car_s, const std::vector<std::vector<double>> &sensor_fusion)
+{
+	bool safe_forward = true;
+	bool safe_backrward = true;
+	bool safe = false;
+	//std::cout << "is_lane_safe" << std::endl;
+	// check if right "middle lane" is safe
+	for (int lane_index = 0; lane_index < sensor_fusion.size(); lane_index++)
+	{
+		float d = sensor_fusion[lane_index][6];
+		if (d < (2 + 4 * target_lane + 2) && d >(2 + 4 * target_lane - 2))
+		{
+			double vx = sensor_fusion[lane_index][3];
+			double vy = sensor_fusion[lane_index][4];
+			double check_speed = sqrt(vx * vx + vy * vy); // get speed magnitude from vx and vy
+			double check_car_s = sensor_fusion[lane_index][5];
+
+			// Looking where other cars in the future
+			check_car_s += ((double)prev_size * 0.02 * check_speed);
+			// using previous points can project s value out 
+			// Check s values greater than mine and s gap
+			// If other car s is greater than our car s "other car is infront of us", and the gap distance between other care s and our casr s is less than 30 menters "too close"
+			std::cout << "car_s: " << car_s << ", check_car_s: "<< check_car_s << std::endl;
+			if (check_car_s > car_s)
+			{
+				std::cout << "Forward distance: " << (check_car_s - car_s) << std::endl;
+			}
+			if ((check_car_s > car_s) && ((check_car_s - car_s) < FORWARD_SAFE_DISRANCE))
+			{
+				// Then we need to take some actions: like lower the speed so we don't crash in to the infront of us
+				// We can raise a flag to try to change the lane
+				safe_forward = false;
+			}
+			if(car_s > check_car_s)
+			{
+				std::cout << "Backward distance: " << (car_s - check_car_s) << std::endl;
+			}
+			if ((car_s > check_car_s) && ((car_s - check_car_s) < BACKWARD_SAFE_DISRANCE))
+			{
+				// Then we need to take some actions: like lower the speed so we don't crash in to the infront of us
+				// We can raise a flag to try to change the lane
+				safe_backrward = false;
+			}
+
+			if (safe_forward && safe_backrward)
+			{
+				safe = true;
+			}
+		}
+	}	// end of: for loop
+	return safe;
 }
 
 #endif  // HELPERS_H
